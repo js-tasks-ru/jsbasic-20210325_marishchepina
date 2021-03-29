01a4f0c6b8f3b65d8475ea57e004528dafd3f8af
Mari Shchepina <marishchepina@gmail.com>
2021-03-30 00:30:34 +0300

    Lesson 1: Basics of JavaScript

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,10 +1,8 @@
 function ucFirst(str) {
   if (str) {
     str = str[0].toUpperCase() + str.slice(1);
-    console.log(str);
     return str;
   } else {
-    console.log("");
     return "";
   }
 }
