a058fea675ba905227e4d7bddad701eb4cb2c573
Mari Shchepina <marishchepina@gmail.com>
2021-04-07 21:16:14 +0300

    Lesson 3: Functions and arrays

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = { min: undefined, max: undefined };
+  let arr = str
+    .split(" ")
+    .join()
+    .split(",")
+    .filter((el) => Number(el) && el);
+  console.log(arr);
+  result.min = Math.min(...arr);
+  result.max = Math.max(...arr);
+  return result;
 }
