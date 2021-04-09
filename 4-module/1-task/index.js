078ff439540f0f4dc120d1b18eea75480f074d61
Mari Shchepina <marishchepina@gmail.com>
2021-04-09 09:26:41 +0300

    task 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+  friends.map((friend) => {
+    const li = document.createElement("li");
+    li.textContent = friend.firstName + friend.lastName;
+    ul.append(li);
+  });
+  return ul;
 }
