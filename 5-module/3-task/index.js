fcccd8efde2626fa3df35ad88030c0697125d911
Mari Shchepina <marishchepina@gmail.com>
2021-04-20 12:03:13 +0300

    Lesson 5: Event handlers

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  const wrapper = document.querySelector(".carousel");
+  const slider = document.querySelector(".carousel__inner");
+  const slides = document.querySelectorAll(".carousel__slide");
+  const prevBtn = document.querySelector(".carousel__arrow_left");
+  const nextBtn = document.querySelector(".carousel__arrow_right");
+  let step = slider.offsetWidth;
+  let counter = 1;
+  let shift = 0;
+
+  counter === 1 && (prevBtn.style.display = "none");
+
+  wrapper.addEventListener("click", function (event) {
+    const carouselArrow = event.target.closest(".carousel__arrow");
+
+    if (carouselArrow) {
+      prevBtn.style.display = "flex";
+      nextBtn.style.display = "flex";
+      if (carouselArrow === nextBtn) {
+        counter++;
+        shift -= step;
+        if (slides.length === counter) {
+          carouselArrow.style.display = "none";
+        }
+      }
+      if (carouselArrow === prevBtn) {
+        counter--;
+        shift += step;
+        if (counter === 1) {
+          carouselArrow.style.display = "none";
+        }
+      }
+      slider.style.transform = "translateX(" + shift + "px)";
+    } else {
+      return;
+    }
+  });
 }
