a1dbf592f6ebfdbac6581340a9aef6723eaeb456
Mari Shchepina <marishchepina@gmail.com>
2021-05-06 23:52:43 +0300

    Lesson 6: JS classes and components

--- 6-module/3-task/task.test.js
+++ 6-module/3-task/task.test.js
@@ -1,7 +1,7 @@
-import slides from './slides.js';
-import Carousel from './index.js';
+import slides from "./slides.js";
+import Carousel from "./index.js";
 
-describe('6-module-3-task', () => {
+describe("6-module-3-task", () => {
   let sut;
 
   let carouselInner;
@@ -17,46 +17,46 @@ describe('6-module-3-task', () => {
     sut = new Carousel(testSlides);
     document.body.append(sut.elem);
 
-    let slideWidth = '500px';
+    let slideWidth = "500px";
 
-    carouselInner = sut.elem.querySelector('.carousel__inner');
+    carouselInner = sut.elem.querySelector(".carousel__inner");
     sut.elem.style.width = slideWidth;
 
-    let slidesElements = sut.elem.querySelectorAll('.carousel__slide');
+    let slidesElements = sut.elem.querySelectorAll(".carousel__slide");
 
     slidesElements.forEach((slideElement) => {
       slideElement.style.width = slideWidth;
-      let img = slideElement.querySelector('.carousel__img');
+      let img = slideElement.querySelector(".carousel__img");
 
       if (img) {
         img.style.width = slideWidth;
       }
     });
 
-    carouselArrowRight = sut.elem.querySelector('.carousel__arrow_right');
-    carouselArrowLeft = sut.elem.querySelector('.carousel__arrow_left');
+    carouselArrowRight = sut.elem.querySelector(".carousel__arrow_right");
+    carouselArrowLeft = sut.elem.querySelector(".carousel__arrow_left");
 
-    clickEvent = new MouseEvent('click', { bubbles: true });
+    clickEvent = new MouseEvent("click", { bubbles: true });
   });
 
   afterEach(() => {
     sut.elem.remove();
   });
 
-  describe('отрисовка вёрстки после создания', () => {
+  describe("отрисовка вёрстки после создания", () => {
     it('должна добавлять корневой элемент в свойство "elem"', () => {
-      expect(sut.elem.classList.contains('carousel')).toBe(true);
+      expect(sut.elem.classList.contains("carousel")).toBe(true);
     });
 
-    it('должна отрисовать все слайды', () => {
-      let slidesElements = sut.elem.querySelectorAll('.carousel__slide');
+    it("должна отрисовать все слайды", () => {
+      let slidesElements = sut.elem.querySelectorAll(".carousel__slide");
 
       expect(slidesElements.length).toBe(3);
     });
   });
 
-  describe('переключение слайдов', () => {
-    describe('переключение вперёд', () => {
+  describe("переключение слайдов", () => {
+    describe("переключение вперёд", () => {
       it('при клике по кнопке "вперёд", должна переключать на один слайд вперёд', () => {
         carouselArrowRight.dispatchEvent(clickEvent);
 
@@ -64,7 +64,7 @@ describe('6-module-3-task', () => {
       });
     });
 
-    describe('переключение назад', () => {
+    describe("переключение назад", () => {
       beforeEach(() => {
         carouselArrowRight.dispatchEvent(clickEvent);
         carouselArrowRight.dispatchEvent(clickEvent);
@@ -73,20 +73,20 @@ describe('6-module-3-task', () => {
       it('при клике по кнопке "назад", должна переключать на один слайд назад', () => {
         carouselArrowLeft.dispatchEvent(clickEvent);
 
-        expect(carouselInner.style.transform).toBe('translateX(-500px)');
+        expect(carouselInner.style.transform).toBe("translateX(-500px)");
       });
     });
 
-    describe('скрытие стрелок переключения', () => {
-      it('в исходном состоянии скрывает стрелку переключения назад', () => {
-        expect(carouselArrowLeft.style.display).toBe('none');
+    describe("скрытие стрелок переключения", () => {
+      it("в исходном состоянии скрывает стрелку переключения назад", () => {
+        expect(carouselArrowLeft.style.display).toBe("none");
       });
 
-      it('при достижении крайнего слайда, должна скрыть стрелку переключения вперёд', () => {
+      it("при достижении крайнего слайда, должна скрыть стрелку переключения вперёд", () => {
         carouselArrowRight.dispatchEvent(clickEvent);
         carouselArrowRight.dispatchEvent(clickEvent);
 
-        expect(carouselArrowRight.style.display).toBe('none');
+        expect(carouselArrowRight.style.display).toBe("none");
       });
     });
   });
@@ -97,37 +97,47 @@ describe('6-module-3-task', () => {
     beforeEach(() => {
       productAddEvent = null;
 
-      document.body.addEventListener('product-add', (event) => {
-        productAddEvent = event;
-      }, { once: true });
+      document.body.addEventListener(
+        "product-add",
+        (event) => {
+          productAddEvent = event;
+        },
+        { once: true }
+      );
     });
 
     afterEach(() => {
       productAddEvent = null;
     });
 
-    it('после клика по кнопке, должно быть создано событие', () => {
-      let addButton = sut.elem.querySelector('.carousel__button');
+    it("после клика по кнопке, должно быть создано событие", () => {
+      let addButton = sut.elem.querySelector(".carousel__button");
       addButton.dispatchEvent(clickEvent);
 
       expect(productAddEvent instanceof CustomEvent).toBe(true);
     });
 
-    it('созданное событие должно содержать в себе уникальный идентификатор товара("id") c 1-ого слайда,' +
-      ' если кликнули на 1 слайд', () => {
-      let addButton = sut.elem.querySelector('.carousel__button');
-      addButton.dispatchEvent(clickEvent);
+    it(
+      'созданное событие должно содержать в себе уникальный идентификатор товара("id") c 1-ого слайда,' +
+        " если кликнули на 1 слайд",
+      () => {
+        let addButton = sut.elem.querySelector(".carousel__button");
+        addButton.dispatchEvent(clickEvent);
 
-      expect(productAddEvent.detail).toBe(testSlides[0].id);
-    });
+        expect(productAddEvent.detail).toBe(testSlides[0].id);
+      }
+    );
 
-    it('созданное событие должно содержать в себе уникальный идентификатор товара("id") c 2-ого слайда,' +
-      ' если кликнули на 2 слайд', () => {
-      carouselArrowRight.dispatchEvent(clickEvent);
-      let addButtons = sut.elem.querySelectorAll('.carousel__button');
-      addButtons[1].dispatchEvent(clickEvent);
+    it(
+      'созданное событие должно содержать в себе уникальный идентификатор товара("id") c 2-ого слайда,' +
+        " если кликнули на 2 слайд",
+      () => {
+        carouselArrowRight.dispatchEvent(clickEvent);
+        let addButtons = sut.elem.querySelectorAll(".carousel__button");
+        addButtons[1].dispatchEvent(clickEvent);
 
-      expect(productAddEvent.detail).toBe(testSlides[1].id);
-    });
+        expect(productAddEvent.detail).toBe(testSlides[1].id);
+      }
+    );
   });
 });
